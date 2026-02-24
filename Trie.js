05058b90830c193e2884b61be88b6a0e9102e536
kma <kma@objctv.ru>
2026-02-24 18:30:43 +0500

    реализовал Trie

--- Trie.js
+++ Trie.js
@@ -19,17 +19,54 @@ class Trie {
 
     insert(word) {
         // TODO Insert word symbol by symbol
+        let currentNode = this.root
 
+        for (let i = 0; i < word.length; i++){
+            const char = word[i]
+
+            if (!currentNode.children[char]){
+                currentNode.children[char] = new TrieNode(char)
+            }
+
+            currentNode = currentNode.children[char]
+        }
+
+        currentNode.isWord = true
     }
 
     hasNode(word){
         // TODO Check is word in Trie
-        return false;
+        let currentNode = this.root;
+
+        for (let i = 0; i < word.length; i++){
+            const char = word[i]
+
+            if (!currentNode.children[char]){
+                return false
+            }
+
+            currentNode = currentNode.children[char]
+        }
+        return currentNode.isWord;
     }
 
     getAllNodes(){
         // TODO returns all nodes as array
-        return [];
+        const nodes = [];
+
+        function traverse(node){
+            if (node.key !== null){
+                nodes.push(node)
+            }
+
+            for (let child in node.children){
+                traverse(node.children[child])
+            }
+        }
+
+        traverse(this.root)
+
+        return nodes
     }
 }
 
