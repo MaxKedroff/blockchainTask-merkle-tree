a01dd5955ee7cadfc3649d0830c331e5daae36e8
kma <kma@objctv.ru>
2026-02-24 18:21:33 +0500

    решил задание Tree.js

--- Tree.js
+++ Tree.js
@@ -18,10 +18,48 @@ class Tree {
 
     addNode(node){
         // TODO 1 Implement 
+        if (this.root === null){
+            this.root = node
+            return
+        }
+        let current = this.root;
+        while (true){
+            if (node.data < current.data)
+            {
+                if (current.left === null){
+                    current.left = node
+                    break
+                }
+
+                current = current.left
+            }
+            else{
+                if (current.right === null){
+                    current.right = node
+                    break
+                }
+                current = current.right       
+            }
+        }
     }
 
     hasNode(data){
         // TODO 2 Implement 
+        let current = this.root
+
+        while (current !== null){
+            if (data === current.data)
+                return true;
+
+            else if (data < current.data){
+                current = current.left
+            }
+            else{
+                current = current.right
+            }
+        }
+
+        return false
     }
 }
 
