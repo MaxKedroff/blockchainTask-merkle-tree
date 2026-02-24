4882cbf3d07ffe170602009d365e89ac86ed6ad4
kma <kma@objctv.ru>
2026-02-24 18:14:47 +0500

    решил задание №2- Transaction.js

--- Transaction.js
+++ Transaction.js
@@ -9,10 +9,20 @@ const utf8ToBytes = require('ethereum-cryptography/utils').utf8ToBytes;
 class Transaction {
     constructor(from, to, value) {
         // TODO 1 Init transaction from, to, value, spent, hash 
+        this.from = from;
+        this.to = to;
+        this.value = value;
+        this.spent = false;
+
+        const hashData = this.from + this.to + this.value;
+        this.hash = SHA256(utf8ToBytes(hashData))
     }
     spend() {
         // TODO 2 Check is transaction spent
-        throw new Error('Already spended!');
+        if (this.spent){
+            throw new Error('Already spended!');
+        }
+        this.spent = true
     }
 }
 
